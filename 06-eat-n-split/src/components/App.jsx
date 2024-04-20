ee40b8be50889c7f20df0b4cca4ba7d4ce99cff8
randymz10 <randymz10@gmail.com>
2024-04-20 19:10:45 -0300

    Finished project

--- 06-eat-n-split/src/components/App.jsx
+++ 06-eat-n-split/src/components/App.jsx
@@ -25,6 +25,19 @@ function App() {
     setShowAddFriend(false);
   }
 
+  function handleSplitBill(value) {
+
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+
+    setSelectedFriend(null);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
@@ -40,7 +53,12 @@ function App() {
           {showAddFriend ? "close" : "Add friend"}
         </Button>
       </div>
-      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
+      )}
     </div>
   );
 }
