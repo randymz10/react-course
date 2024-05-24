59dd454bb4fc1fecd9d6f6f565028aa9b162e211
randymz10 <randymz10@gmail.com>
2024-05-24 00:35:33 -0300

    Added new component

--- 07-usepopcorn/src/main.jsx
+++ 07-usepopcorn/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
-import './index.css'
+import StarRating from './components/StarRating'
+// import App from './components/App.jsx'
+// import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5}/>
+    <StarRating maxRating={10}/>
   </React.StrictMode>,
 )
