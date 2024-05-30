21bd9e33de458fdd3020cc461eb4b651599ff8ee
randymz10 <randymz10@gmail.com>
2024-05-30 00:22:39 -0300

    finished app

--- 07-usepopcorn/src/main.jsx
+++ 07-usepopcorn/src/main.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import StarRating from './components/StarRating'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./components/StarRating";
 // import App from './components/App.jsx'
 // import './index.css'
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
-  </React.StrictMode>,
-)
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
+  </React.StrictMode>
+);
