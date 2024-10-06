1123af082285389034685f34520f791d61468bf0
randymz10 <randymz10@gmail.com>
2024-10-06 00:41:11 -0300

    Added new components

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -14,8 +14,12 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>form</p>}/>
+        </Route>
         <Route path="*" element={<PageNotFound />} />
-        <Route path="app" element={<AppLayout />} />
       </Routes>
     </BrowserRouter>
   );
