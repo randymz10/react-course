f4aa51c7218cf47cc82c249dbf70e7b876e3f5b8
randymz10 <randymz10@gmail.com>
2024-10-10 22:07:04 -0300

    Added the country list

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -6,7 +8,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -44,7 +46,7 @@ function App() {
           />
           <Route
             path="countries"
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={<CountryList cities={cities} isLoading={isLoading} />}
           />
           <Route path="form" element={<p>form</p>} />
         </Route>
