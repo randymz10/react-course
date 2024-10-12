277265f8992f2a54fa12de82fcfc9dd9870f411a
randymz10 <randymz10@gmail.com>
2024-10-11 23:45:04 -0300

    Learned about query params and userParams hook of react router dom

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,11 +1,19 @@
-import styles from './Map.module.css'
+import { useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
 
 function Map() {
-    return (
-        <div className={styles.mapContainer}>
-            Map
-        </div>
-    )
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h2>
+        Position: {lat}, {lng}
+      </h2>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
