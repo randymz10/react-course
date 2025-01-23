03a25d54d7b290ac4f22ba3b37540c1d09a63bda
randymz10 <randymz10@gmail.com>
2025-01-23 20:54:23 -0300

    Added new styles in the appLayout mainly

--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">John Doe</div>;
+  return <div className="hidden text-sm font-semibold md:block">John Doe</div>;
 }
 
 export default Username;
