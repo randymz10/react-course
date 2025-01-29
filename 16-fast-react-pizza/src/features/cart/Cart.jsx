77edb6014b8ab2fe7a2a7a1ba49bc572152e73d7
randymz10 <randymz10@gmail.com>
2025-01-29 20:02:31 -0300

    Added styles to the Cart into the Fast React Pizza Co. project

--- 16-fast-react-pizza/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Button from "../../ui/Button";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -29,7 +30,7 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <Link
         to="/menu"
         className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
@@ -37,13 +38,19 @@ function Cart() {
         &larr; Back to menu
       </Link>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <button>Clear cart</button>
+        <Button type='seconday'>Clear cart</Button>
       </div>
     </div>
   );
