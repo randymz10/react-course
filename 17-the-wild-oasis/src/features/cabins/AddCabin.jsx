8869bb79841592fc1a90783a3fbdc422cd8bbab6
randymz10 <randymz10@gmail.com>
2025-05-17 02:13:07 -0300

    Finished lecture about  'Converting the Modal to a Compound Component' into the widl oasis project

--- 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,27 +1,51 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
-import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
+import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button
-        variation="primary"
-        size="medium"
-        onClick={() => setIsOpenModal((show) => !show)}
-      >
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button variation="primary" size="medium">
+          Add new cabin
+        </Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button variation="primary" size="medium">
+          Show table
+        </Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button
+//         variation="primary"
+//         size="medium"
+//         onClick={() => setIsOpenModal((show) => !show)}
+//       >
+//         Add new cabin
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
