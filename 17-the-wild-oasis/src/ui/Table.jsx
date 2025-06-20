aed8a23cd78c1b51913d7c0f6cc626cbe6cf6fb7
randymz10 <randymz10@gmail.com>
2025-06-20 11:18:31 -0300

    Add filtering functionality to CabinTable and implement CabinTableOperations component

--- 17-the-wild-oasis/src/ui/Table.jsx
+++ 17-the-wild-oasis/src/ui/Table.jsx
@@ -87,7 +87,7 @@ function Row({ children }) {
   );
 }
 function Body({ data, render }) {
-  if (!data.length) return <Empty>No data</Empty>;
+  if (!data?.length) return <Empty>No data</Empty>;
   return <StyledBody>{data.map(render)}</StyledBody>;
 }
 
